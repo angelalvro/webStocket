f71498b856e6d22d67defca74de822b7b74bbe8f
angelal.93@hotmail.es <angelal.93@hotmail.es>
2022-04-29 12:33:09 +0200

    Proyecto Finalizado

--- server/server.js
+++ server/server.js
@@ -2,67 +2,61 @@ var WebSocketServer = require('ws').Server;
 wss = new WebSocketServer({ port: 9001 });
 
 //array para almacenar las figuras 
-var elementos = { "objects": [] };
-var persons;
+var elementos = { "objects": [] , "clientsId":[]};
 
 wss.on('connection', function connection(ws) {
     ws.id = wss.getUniqueID();
 
     wss.clients.forEach(function each(client) {
         console.log('Client.ID: ' + client.id);
+        if(!elementos.clientsId.includes(client.id)){
+            elementos.clientsId.push(client.id);
+            console.log(' elementos.clientsId: ' +  elementos.clientsId);
+        }
     });
 
     ws.on('message', function incoming(message) {
         if (isJson(message)) {
             var obj = JSON.parse(message);
             
-            //if(!elementos.objects.includes(message)){
-                console.log('Añado el objeto: %s', JSON.stringify(obj));
-                //Funaciona:
+            console.log('obj: %s', JSON.stringify(obj));
+
+            //Cuando movemos enviamos todo el canvas de nuevo
+            if (elementos.objects.filter(function(e) { return e.id === obj.id; }).length > 0) {
+                elementIndex = elementos.objects.findIndex((elemento => elemento.id == obj.id));
+                elementos.objects[elementIndex] = obj;
+
+                wss.broadcast(message, this);
+            }else{
                 elementos.objects.push(obj)
-                //elementos.objects.push(obj[0])
-                //Array.prototype.push.apply(elementos.objects, obj);
-                console.log('Todos los objetos son: %s', JSON.stringify(elementos));
+
                 wss.broadcast(message, this);
                 console.log('broadcasting data');
-                console.log('obj: %s', JSON.stringify(obj));
-            //}
-            
-            //if (currentSlideData.indexv != obj.indexv || currentSlideData.indexh != obj.indexh) {
-            //actualizar información de currentSlideData
-            //}
+            }
         }
         console.log('received: %s', message);
     });
     console.log('sending initial Data %s', JSON.stringify(elementos));
-    //mandar datos iniciales de sincronización
-    if(elementos.objects.length != 0){
-        ws.send(JSON.stringify(elementos));
-    }
+
+    ws.send(JSON.stringify(elementos));
 });
 
 wss.broadcast = function broadcast(data, sentBy) {
-    console.log('data: %s', data);
-    console.log('sentBy.id: %s', sentBy.id);
-    console.log('this.clients: %s', this.clients);
-
     this.clients.forEach(function (cl) {
-        console.log('Entra en clientes');
-        console.log('cl.id: %s', cl.id);
         if (sentBy.id != cl.id) {
-            var figura = { "objects": [] };
+            var figura = { "objects": [] , "clientsId":[]};
             var ultimoObj = [];
             
             var obj = JSON.parse(data);
             ultimoObj.push(obj)
             Array.prototype.push.apply(figura.objects, ultimoObj);
-            //Por aqui
-            console.log('obj: %s', JSON.stringify(obj));
-            console.log('obj[obj.length-1]: %s', JSON.stringify(obj[obj.length-1]));
-            console.log('ultimoObj: %s', ultimoObj);
-            //Array.prototype.push.apply(figura.objects, ultimoObj);
-            console.log('Envia datos para el cl.id: %s', cl.id);
-            console.log('JSON.stringify(figura): %s', JSON.stringify(figura));
+
+            wss.clients.forEach(function each(client) {
+                if(!figura.clientsId.includes(client.id)){
+                    figura.clientsId.push(client.id);
+                }
+            });
+
             cl.send(JSON.stringify(figura));
         }
     })
